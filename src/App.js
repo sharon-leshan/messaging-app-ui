fd25fcdbb0386736a01ef48ad3ac0cfaa18b3af4
sharon-leshan <maleos380@gmail.com>
2021-12-02 00:37:53 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
 import React, { useEffect, useState} from 'react'
 import Pusher from 'pusher-js'
+import axios from './components/axios'
 
 function App() {
   const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    axios.get("/messages/sync").then(res => {
+      setMessages(res.data)
+    })
+  }, [])
 
   useEffect(() => {
     const pusher = new Pusher('', {
@@ -28,7 +35,7 @@ function App() {
     <div className="app">
       <div className="app__body">
         <Sidebar />
-        <Chat />
+        <Chat messages={messages}/>
       </div>
     </div>
   );
