da61a4152d88168cec7591607d9185902e7e1f06
sharon-leshan <maleos380@gmail.com>
2021-12-01 15:24:02 +0300

    format

--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -1,23 +1,23 @@
-import React, {useEffect, useState} from 'react';
-import {Avatar} from '@material-ui/core'
-import './SidebarChat.css'
+import React, { useEffect, useState } from "react";
+import { Avatar } from "@material-ui/core";
+import "./SidebarChat.css";
 
 const SidebarChat = () => {
-    const [seed, setSeed] = useState("")
+  const [seed, setSeed] = useState("");
 
-    useEffect(() => {
-        setSeed(Math.floor(Math.random()* 5000))
-    }, [])
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
 
-    return(
-        <div className="sidebarChat">
-            <Avatar src={`https://avatars.dicebear.com/api/human/b${seed}.svg`} />
-            <div className="sidebarChat__info" >
-                <h2>Room name</h2>
-                <p>Last message...</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="sidebarChat">
+      <Avatar src={`https://avatars.dicebear.com/api/human/b${seed}.svg`} />
+      <div className="sidebarChat__info">
+        <h2>Room name</h2>
+        <p>Last message...</p>
+      </div>
+    </div>
+  );
+};
 
-export default SidebarChat
+export default SidebarChat;
