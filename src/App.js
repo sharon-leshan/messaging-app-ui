dae0837aeb94180f1331412b9456e345fc50cfa2
sharon-leshan <maleos380@gmail.com>
2021-12-01 23:49:58 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import React, { useEffect, useState} from 'react'
+import Pusher from 'pusher-js'
 
 function App() {
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    const pusher = new Pusher('', {
+      cluster: 'ap2'
+  });
+
+  const channel = pusher.subscribe('messages');
+  channel.bind('inserted', (data) => {
+    setMessages([...messages, data])
+  });
+  
+  return () => {
+    channel.unbind_all()
+    channel.unsubscribe()
+  }
+}, [messages])
+
+  console.log(messages)
   return (
     <div className="app">
       <div className="app__body">
