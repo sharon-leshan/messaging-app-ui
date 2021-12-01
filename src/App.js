2d34889de75e1d81eb826f9150373cc52436336f
sharon-leshan <maleos380@gmail.com>
2021-12-01 15:19:55 +0300

    format

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import Sidebar from './components/Sidebar';
-import Chat from './components/Chat'
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <div className="app">
-      <div className="app__body" >
-          <Sidebar/>
-          <Chat/>
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
       </div>
     </div>
   );
