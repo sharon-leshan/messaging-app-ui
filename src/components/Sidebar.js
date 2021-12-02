debaeb678a2326ef48f7d4dfb80d53d444ef8663
sharon-leshan <maleos380@gmail.com>
2021-12-02 10:08:20 +0300

    updated

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,12 +6,15 @@ import { Avatar, IconButton } from "@material-ui/core";
 import { SearchOutlined, MoreVert } from "@material-ui/icons";
 import SidebarChat from "./SidebarChat";
 import "./Searchbar.css";
+import {useStateValue} from './StateProvider';
 
-const Sidebar = () => {
+const Sidebar = ({messages}) => {
+  const [{user}, dispatch]= useStateValue()
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar src="https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90b" />
+        <Avatar src={user?.photoURL} />
         <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon />
@@ -31,9 +34,7 @@ const Sidebar = () => {
         </div>
       </div>
       <div className="sidebar__chats">
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
+        <SidebarChat messages={messages} />
       </div>
     </div>
   );
