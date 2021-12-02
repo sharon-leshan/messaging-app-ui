1f5b16c78892d99b15da435b5e9a102f3187af10
sharon-leshan <maleos380@gmail.com>
2021-12-02 15:40:05 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,48 +1,49 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
-import React, { useEffect, useState} from 'react'
-import Pusher from 'pusher-js'
-import axios from './components/axios'
-import Login from './components/Login';
-import {useStateValue} from './components/StateProvider';
+import React, { useEffect, useState } from "react";
+import Pusher from "pusher-js";
+import axios from "./components/axios";
+import Login from "./components/Login";
+import { useStateValue } from "./components/StateProvider";
 
 function App() {
-  const [messages, setMessages] = useState([])
-  // const [user, setUser] = useState(null)
-  const [{user}] = useStateValue()
+  const [messages, setMessages] = useState([]);
+  const [{ user }, dispatch] = useStateValue();
 
   useEffect(() => {
-    axios.get("/messages/sync").then(res => {
-      setMessages(res.data)
-    })
-  }, [])
+    axios.get("/messages/sync").then((res) => {
+      setMessages(res.data);
+    });
+  }, []);
 
   useEffect(() => {
-    const pusher = new Pusher('', {
-      cluster: 'ap2'
-  });
+    const pusher = new Pusher("730b2a8c8285ec3a6355", {
+      cluster: "mt1",
+    });
 
-  const channel = pusher.subscribe('messages');
-  channel.bind('inserted', (data) => {
-    setMessages([...messages, data])
-  });
-  
-  return () => {
-    channel.unbind_all()
-    channel.unsubscribe()
-  }
-}, [messages])
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      setMessages([...messages, data]);
+    });
 
-  console.log(messages)
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
 
   return (
     <div className="app">
-      {!user ? <Login/> : (
+      {!user ? (
+        <Login />
+      ) : (
         <div className="app__body">
-        <Sidebar messages={messages} />
-        <Chat messages={messages}/>
-      </div>
+          <Sidebar messages={messages} />
+          <Chat messages={messages} />
+        </div>
       )}
     </div>
   );
