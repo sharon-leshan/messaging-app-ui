56d7ebafd747c0f6c5e0a4d122cbda5732a1bf50
sharon-leshan <maleos380@gmail.com>
2021-12-02 10:09:26 +0300

    updated

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -10,16 +10,18 @@ import MicIcon from "@material-ui/icons/Mic";
 import "./Chat.css";
 import "./Chat.css";
 import axios from "axios";
+import {useStateValue} from './StateProvider';
 
 const Chat = ({messages}) => {
   const [seed, setSeed] = useState("");
   const [input, setInput] = useState("")
+  const [{user}, dispatch] = useStateValue()
 
   const sendMessage = async (e) => {
     e.preventDefault()
     await axios.post('/messages/new', {
-      message: InputAdornment,
-      name: "thewebdev",
+      message: input,
+      name: user.displayname,
       timestamp:new Date().toUTCString(),
       received: true
     })
@@ -28,13 +30,14 @@ const Chat = ({messages}) => {
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
   return (
     <div className="chat">
       <div className="chat__header">
         <Avatar src={`https://avatars.dicebear.com/api/human/b${seed}.svg`} />
         <div className="chat__headerInfo">
           <h3>Maleo</h3>
-          <p>Last seen at...</p>
+          <p>Last seen at{""} {messages[messages.length -1]?.timestamp} </p>
         </div>
         <div className="chat__headerRight">
           <IconButton>
@@ -50,7 +53,7 @@ const Chat = ({messages}) => {
       </div>
       <div className="chat__body">
       {messages.map(message => (
-            <p className={`chat__message ${message.received && 'chat__receiver'}`}>
+            <p className={`chat__message ${message.name === user.displayname && 'chat__receiver'}`}>
               <span className="chat__name">{message.name} </span>
               {message.message}
               <span className="chat__timestamp">
