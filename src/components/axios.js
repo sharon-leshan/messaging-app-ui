d2a6112b1eeb5eb10bdb56adb20a61355829e00a
sharon-leshan <maleos380@gmail.com>
2021-12-02 15:44:10 +0300

    heroku url

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: " https://messaging-app-backend-maleo.herokuapp.com",
 });
 
 export default instance;
