dd9edf2eb63cf76b0479d8e9bd3cebf57c89c223
sharon-leshan <maleos380@gmail.com>
2021-12-02 15:38:42 +0300

    updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,33 @@
 import React from "react";
-import {Button} from '@material-ui/core'
-import './Login.css'
-import {auth, provider} from '../firebase'
-import {actionTypes} from './reducer'
-import {useStateValue} from './StateProvider'
+import { Button } from "@material-ui/core";
+import "./Login.css";
+import { auth, provider } from "../firebase";
+import { actionTypes } from "./reducer";
+import { useStateValue } from "./StateProvider";
 
 const Login = () => {
-    const [{}, dispatch] = useStateValue()
+  const [{}, dispatch] = useStateValue();
 
-    const signIn = () => {
-        auth.signInWithPopup(provider)
-        .then(result => {
-            dispatch({
-                type: actionTypes.SET_USER,
-                user: result.user
-            })
-        })
-        .catch(error => alert(error.message))
-    }
-    return (
-        <div className="login__container" >
-            <img src="logo512.png" alt="whatsapp" />
-            <div className="login__text" >
-                <h1>Sign in to Messaging App</h1>
-            </div>
-            <Button onClick={signIn} >Sign in with Google</Button>
-        </div>
-    )
-}
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        });
+      })
+      .catch((error) => alert(error.message));
+  };
+  return (
+    <div className="login__container">
+      <img src="logo512.png" alt="whatsapp" />
+      <div className="login__text">
+        <h1>Sign in to Messaging App</h1>
+      </div>
+      <Button onClick={signIn}>Sign in with Google</Button>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
